84cd623f515705acdaa9766a6cbcdb9fdc192431
Elizaveta <elizaveta@cutie.dell>
2017-11-08 00:29:45 +0300

    hw4

--- week5/hw4.rkt
+++ week5/hw4.rkt
@@ -16,15 +16,59 @@
 )
 
 (define (list-nth-mod xs n)
-  (if
-     (null? xs)
-     (error "list-nth-word: empty list")
-     (let
+  (cond
+     [(< n 0)
+     (error "list-nth-mod: negative number")]
+     [(null? xs)
+     (error "list-nth-mod: empty list")]
+     [#t (let
          ([n_remainder (remainder n (length xs))])
          (car (list-tail xs n_remainder))
-         )
+         )]
+  )
+)
+
+(define (stream-for-n-steps s n)
+  (if
+   (= n 0)
+   null
+  (let ([s1 (s)])
+     (cons
+      (car s1)
+      (stream-for-n-steps (cdr s1) (- n 1))
+     )
+  )
+  )
+)
+(define funny-number-stream
+  (letrec ([f (lambda (n)
+             (cons
+              (if (= (remainder n 5) 0) (- 0 n) n)
+              (lambda () (f (+ n 1)))
+              ))
+       ])
+    (lambda ()
+      (f 1)
+    )
   )
 )
 
 
-stream-for-
+(define dan-then-dog
+  (letrec
+      ([dan (lambda () (cons "dan.jpg" dog))]
+       [dog (lambda () (cons "dog.jpg" dan))]
+       )
+   dan
+  )
+)
+
+(define (stream-add-zero s)
+  (let
+      (
+       [f (lambda ())]
+       []
+       )
+   )
+  
+)
