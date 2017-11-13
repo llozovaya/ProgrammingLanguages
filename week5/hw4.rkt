400298cf895d5d2c1d02d8f6c279544758c9ab03
Elizaveta Lozovaya <lozovaya@bifit.com>
2017-11-13 10:31:28 +0300

    конец hw4

--- week5/hw4.rkt
+++ week5/hw4.rkt
@@ -2,7 +2,7 @@
 
 (provide (all-defined-out))
 
-
+; Problem 1
 (define (sequence low high stride)
   (if
    (<= low high)
@@ -11,10 +11,12 @@
    )
 )
 
+; Problem 2
 (define (string-append-map xs suffix)
   (map (lambda (str)  (string-append str suffix) ) xs)
 )
 
+; Problem 3
 (define (list-nth-mod xs n)
   (cond
      [(< n 0)
@@ -28,6 +30,7 @@
   )
 )
 
+; Problem 4
 (define (stream-for-n-steps s n)
   (if
    (= n 0)
@@ -40,6 +43,8 @@
   )
   )
 )
+
+; Problem 5
 (define funny-number-stream
   (letrec ([f (lambda (n)
              (cons
@@ -53,7 +58,7 @@
   )
 )
 
-
+; Pronlem 6
 (define dan-then-dog
   (letrec
       ([dan (lambda () (cons "dan.jpg" dog))]
@@ -63,12 +68,55 @@
   )
 )
 
+; Problem 7
 (define (stream-add-zero s)
-  (let
-      (
-       [f (lambda ())]
-       []
-       )
-   )
-  
+  (lambda ()
+    (let ([s1 (s)])
+      (cons (cons 0 (car s1)) (stream-add-zero (cdr s1)))
+    )
+  )
 )
+
+
+; Problem 8
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (n) (cons (cons (list-nth-mod xs n) (list-nth-mod ys n)) (lambda () (f (+ n 1)))) )])
+   (lambda ()
+    (f 0)
+    )
+  )
+)
+
+; Problem 9
+(define (vector-assoc v vec)
+  (letrec ([len (vector-length vec)]
+           [f (lambda (i) (let ([vi (vector-ref vec i)])
+                           (if (and (pair? vi) (= (car vi) v))
+                               vi
+                               (if (= i (- len 1))
+                                  #f
+                                  (f (+ i 1))
+                               )
+                           ) ))])  
+  (f 0)
+  )
+)
+
+; Problem 10
+(define (cached-assoc xs n)
+  (letrec ([cache (make-vector n #f)]
+           [pointer 0]
+           )
+   (lambda (v)
+    (let ([cached (vector-assoc v cache)])
+      (if cached cached
+        (let ([ans (assoc v xs)])
+          (if ans
+          (begin (vector-set! cache pointer ans) (set! pointer (if (= pointer (- n 1)) 0 (+ pointer 1)) ) ans)
+          ans
+          )
+          )
+      )
+    )
+   )
+))
